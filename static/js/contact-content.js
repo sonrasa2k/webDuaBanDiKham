e3e0ea9f34a710a97724fabae9289f26c525dc4a
unknown <sonrasa2k>
2025-08-23 00:57:53 +0700

    fix hero

--- static/js/contact-content.js
+++ static/js/contact-content.js
@@ -62,13 +62,6 @@ export function renderContactContent() {
                   <p><a href="mailto:lienhe@duubandikham.vn">lienhe@duubandikham.vn</a></p>
                 </div>
               </div>
-              <div class="contact-info-item">
-                <i class="fas fa-clock"></i>
-                <div>
-                  <h5>Giờ Làm Việc</h5>
-                  <p>Thứ 2 - Thứ 7: 7:00 - 19:00</p>
-                </div>
-              </div>
               <div class="mt-4">
                 <h5>Mạng Xã Hội</h5>
                 <div class="social-links">
@@ -85,7 +78,7 @@ export function renderContactContent() {
     </section>
 
     <section class="map-container">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3920.08335515441!2d106.62172972675495!3d10.72805421008065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752d55d146b0c3%3A0xe9a03dbd3ae7bf44!2sCity%20gate%20Towers!5e0!3m2!1sen!2s!4v1755885380370!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3920.08335515441!2d106.62172972675495!3d10.72805421008065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752d55d146b0c3%3A0xe9a03dbd3ae7bf44!2sCity%20gate%20Towers!5e0!3m2!1sen!2s!4v1755885380370!5m2!1sen!2s" width=100% height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </section>
   `;
 }
